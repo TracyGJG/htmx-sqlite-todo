18ae5a4cbda19ef77809841618b2faa1fd225987
TGJ Gilmore <TracyGJG@users.noreply.github.com>
2024-10-11 17:20:00 +0100

    Use ids instead of name

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
-import { addToDo, getToDosList, updateToDo, dropToDo } from "./toDo.js";
+import { addToDo, getToDosList, updateToDo, dropToDo } from './toDo.js';
 
 const app = express();
 const PORT = process.env.PORT || 3456;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/todos", (req, res) => {
-  setTimeout(() => {
-    res.send(getToDosList());
-  }, 2000); // Artificial delay to show indicator.
+app.get('/todos', (req, res) => {
+	setTimeout(() => {
+		res.send(getToDosList());
+	}, 2000); // Artificial delay to show indicator.
 });
 
-app.post("/todos", (req, res) => {
-  res.send(
-    addToDo({
-      name: req.body.newToDo,
-      done: false,
-    })
-  );
+app.post('/todos', (req, res) => {
+	res.send(
+		addToDo({
+			name: req.body.newToDo,
+			done: false,
+		})
+	);
 });
 
-app.put("/todos/:name", (req, res) => {
-  res.send(updateToDo(req.params.name, req.query.done === "false"));
+app.put('/todos/:name', (req, res) => {
+	res.send(updateToDo(req.params.name, req.query.done === 'false'));
 });
 
-app.delete("/todos/:name", (req, res) => {
-  res.send(dropToDo(req.params.name));
+app.delete('/todos/:id', (req, res) => {
+	res.send(dropToDo(req.params.id));
 });
 
 app.listen(PORT, () => {
-  console.log(`http://127.0.0.1:${PORT}`);
+	console.log(`http://127.0.0.1:${PORT}`);
 });
--- toDo.js
+++ toDo.js
@@ -1,55 +1,60 @@
-import Database from "better-sqlite3";
+import Database from 'better-sqlite3';
 
-const db = new Database("todos.db", { verbose: console.log });
-db.pragma("journal_mode = WAL");
+const db = new Database('todos.db', { verbose: console.log });
+db.pragma('journal_mode = WAL');
 
 // Template Functions
-export const toDoDone = (toDo) => /*html*/ `
+export const toDoDone = toDo => /*html*/ `
 <input
     type="checkbox"
-    id="${toDo.name}"
+    id="${toDo.id}"
     name="${toDo.name}"
-    ${toDo.done ? "checked" : ""}
-    hx-put="/todos/${toDo.name}?done=${toDo.done}"
+    ${toDo.done ? 'checked' : ''}
+    hx-put="/todos/${toDo.id}?done=${toDo.done}"
     hx-swap="outerHTML"
     hx-indicator=".htmx-indicator"/>`;
 
-const toDoEntry = (toDo) => /*html*/ `<li>
+const toDoEntry = toDo => /*html*/ `<li>
   ${toDoDone(toDo)}
   <label for="${toDo.name}">${toDo.name}</label>
-  <button hx-delete="/todos/${toDo.name}" hx-target="closest li">del</button>
+  <button hx-delete="/todos/${toDo.id}" hx-target="closest li">del</button>
 </li>`;
 
 // Database Functions
-const getAllToDos = () => {
-  const result = db.prepare(`SELECT * FROM toDos`).all();
-
-  return result.map((row) => ({
-    name: row.name,
-    done: row.done === "TRUE",
-  }));
-};
-
 export function getToDosList() {
-  return getAllToDos().map(toDoEntry).join("");
+	const result = db.prepare(`SELECT * FROM toDos`).all();
+	console.table(result);
+	return result
+		.map(row =>
+			toDoEntry({
+				id: row.id,
+				name: row.name,
+				done: row.done === 'TRUE',
+			})
+		)
+		.join('');
 }
 
 export function addToDo(toDo) {
-  const stmt = db.prepare(
-    "INSERT INTO toDos (name, done) VALUES (:name, :done)"
-  );
-  stmt.run({ name: toDo.name, done: `${toDo.done}`.toUpperCase() });
-  getAllToDos();
-  return toDoEntry(toDo);
+	const stmt = db.prepare(
+		'INSERT INTO toDos (name, done) VALUES (:name, :done) Returning RowId'
+	);
+	toDo.id = stmt.run({
+		name: toDo.name,
+		done: `${toDo.done}`.toUpperCase(),
+	}).lastInsertRowid;
+	return toDoEntry(toDo);
 }
 
-export function updateToDo(name, _done) {
-  const stmt = db.prepare("UPDATE toDos SET done = :done WHERE name = :name");
-  stmt.run({ name, done: `${_done}`.toUpperCase() });
-  return toDoDone({ name, done: _done });
+export function updateToDo(id, _done) {
+	const stmt = db.prepare(
+		'UPDATE toDos SET done = :done WHERE id = :id Returning name'
+	);
+	const name = stmt.run({ id, done: `${_done}`.toUpperCase() });
+	return toDoDone({ id, name, done: _done });
 }
 
-export function dropToDo(toDoName) {
-  const stmt = db.prepare("DELETE FROM toDos WHERE name = :name");
-  stmt.run({ name: toDoName });
+export function dropToDo(toDoId) {
+	const stmt = db.prepare('DELETE FROM toDos WHERE id = :id');
+	stmt.run({ id: toDoId });
 }
